d8b433fdf1c2bb1fc096d116a36d154efbdc5c49
Abhishek Khangare <113746515+AbhishekKhangare21@users.noreply.github.com>
2023-05-23 09:32:25 +0530

    Update LightBox.js

--- components/LightBox.js
+++ components/LightBox.js
@@ -9,7 +9,6 @@ const LightBox = ({ clickedImg, setClickedImg }) => {
     <>
       <div className="overlay dismiss" onClick={handleClick}>
         <img src={clickedImg.imageUrl} alt="bigger pic" />
-        <p className="bg-gray-300 p-2 ">{clickedImg.description}</p>
 
         <span className="dismiss" onClick={handleClick}>
           X
